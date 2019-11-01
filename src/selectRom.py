50aaa23bbe60d5b638ac84ff0f624a11f9eb1235
LiteApplication <37189150+LiteApplication@users.noreply.github.com>
2019-11-01 17:17:28 +0100

    Update selectRom.py

--- src/selectRom.py
+++ src/selectRom.py
@@ -38,7 +38,7 @@ def main(runDir="NoDir"):
         exit(2)
     else:
         rom=availableRom[0]
-    os.system(srcPath+"romExtractor.sh", runDir+"/"+rom, srcPath)
+    os.system(srcPath+"/romExtractor.sh", runDir+"/"+rom, srcPath)
 def chooseFile(files):
     for i,file in zip(range(1,len(files)),files):
         print("\t"+i+" : "+file)
