a87f68a1a6de71fbf1e484e68c994b2da35ace24
LiteApplication <contact.liteapplication@gmail.com>
2019-12-03 16:19:00 +0100

    Make the program quiet

--- src/sdat2img.py
+++ src/sdat2img.py
@@ -8,6 +8,10 @@
 
 from __future__ import print_function
 import sys, os, errno
+def printv(msg):
+    if v:
+        open(sys.argv[5],"a").write(msg)
+        print(msg)
 
 def main(TRANSFER_LIST_FILE, NEW_DATA_FILE, OUTPUT_IMAGE_FILE):
     __version__ = '1.2'
@@ -20,13 +24,13 @@ def main(TRANSFER_LIST_FILE, NEW_DATA_FILE, OUTPUT_IMAGE_FILE):
         input('Press ENTER to exit...')
         sys.exit(1)
     else:
-        print('sdat2img binary - version: {}\n'.format(__version__))
+        printv('sdat2img binary - version: {}\n'.format(__version__))
 
     def rangeset(src):
         src_set = src.split(',')
         num_set =  [int(item) for item in src_set]
         if len(num_set) != num_set[0]+1:
-            print('Error on parsing following data to rangeset:\n{}'.format(src), file=sys.stderr)
+            printv('Error on parsing following data to rangeset:\n{}'.format(src), file=sys.stderr)
             sys.exit(1)
 
         return tuple ([ (num_set[i], num_set[i+1]) for i in range(1, len(num_set), 2) ])
@@ -56,7 +60,7 @@ def main(TRANSFER_LIST_FILE, NEW_DATA_FILE, OUTPUT_IMAGE_FILE):
             else:
                 # Skip lines starting with numbers, they are not commands anyway
                 if not cmd[0].isdigit():
-                    print('Command "{}" is not valid.'.format(cmd), file=sys.stderr)
+                    printv('Command "{}" is not valid.'.format(cmd), file=sys.stderr)
                     trans_list.close()
                     sys.exit(1)
 
@@ -68,24 +72,24 @@ def main(TRANSFER_LIST_FILE, NEW_DATA_FILE, OUTPUT_IMAGE_FILE):
     version, new_blocks, commands = parse_transfer_list_file(TRANSFER_LIST_FILE)
 
     if version == 1:
-        print('Android Lollipop 5.0 detected!\n')
+        printv('Android Lollipop 5.0 detected!\n')
     elif version == 2:
-        print('Android Lollipop 5.1 detected!\n')
+        printv('Android Lollipop 5.1 detected!\n')
     elif version == 3:
-        print('Android Marshmallow 6.x detected!\n')
+        printv('Android Marshmallow 6.x detected!\n')
     elif version == 4:
-        print('Android Nougat 7.x / Oreo 8.x detected!\n')
+        printv('Android Nougat 7.x / Oreo 8.x detected!\n')
     else:
-        print('Unknown Android version!\n')
+        printv('Unknown Android version!\n')
 
     # Don't clobber existing files to avoid accidental data loss
     try:
         output_img = open(OUTPUT_IMAGE_FILE, 'wb')
     except IOError as e:
         if e.errno == errno.EEXIST:
-            print('Error: the output file "{}" already exists'.format(e.filename), file=sys.stderr)
-            print('Remove it, rename it, or choose a different file name.', file=sys.stderr)
-            sys.exit(e.errno)
+            printv('File already exist')
+            os.remove(OUTPUT_IMAGE_FILE)
+            output_img = open(OUTPUT_IMAGE_FILE, 'wb')
         else:
             raise
 
@@ -99,7 +103,7 @@ def main(TRANSFER_LIST_FILE, NEW_DATA_FILE, OUTPUT_IMAGE_FILE):
                 begin = block[0]
                 end = block[1]
                 block_count = end - begin
-                print('Copying {} blocks into position {}...'.format(block_count, begin))
+                printv('Copying {} blocks into position {}...'.format(block_count, begin))
 
                 # Position output file
                 output_img.seek(begin*BLOCK_SIZE)
@@ -109,7 +113,7 @@ def main(TRANSFER_LIST_FILE, NEW_DATA_FILE, OUTPUT_IMAGE_FILE):
                     output_img.write(new_data_file.read(BLOCK_SIZE))
                     block_count -= 1
         else:
-            print('Skipping command {}...'.format(command[0]))
+            printv('Skipping command {}...'.format(command[0]))
 
     # Make file larger if necessary
     if(output_img.tell() < max_file_size):
@@ -117,18 +121,18 @@ def main(TRANSFER_LIST_FILE, NEW_DATA_FILE, OUTPUT_IMAGE_FILE):
 
     output_img.close()
     new_data_file.close()
-    print('Done! Output image: {}'.format(os.path.realpath(output_img.name)))
+    printv('Done! Output image: {}'.format(os.path.realpath(output_img.name)))
 
 if __name__ == '__main__':
     try:
         TRANSFER_LIST_FILE = str(sys.argv[1])
         NEW_DATA_FILE = str(sys.argv[2])
     except IndexError:
-        print('\nUsage: sdat2img.py <transfer_list> <system_new_file> [system_img]\n')
-        print('    <transfer_list>: transfer list file')
-        print('    <system_new_file>: system new dat file')
-        print('    [system_img]: output system image\n\n')
-        print('Visit xda thread for more information.\n')
+        printv('\nUsage: sdat2img.py <transfer_list> <system_new_file> [system_img]\n')
+        printv('    <transfer_list>: transfer list file')
+        printv('    <system_new_file>: system new dat file')
+        printv('    [system_img]: output system image\n\n')
+        printv('Visit xda thread for more information.\n')
         try:
             input = raw_input
         except NameError: pass
@@ -139,5 +143,9 @@ if __name__ == '__main__':
         OUTPUT_IMAGE_FILE = str(sys.argv[3])
     except IndexError:
         OUTPUT_IMAGE_FILE = 'system.img'
-
+    try:
+        if sys.argv[4] == "-v" :
+            v=True
+        else:
+            v=False
     main(TRANSFER_LIST_FILE, NEW_DATA_FILE, OUTPUT_IMAGE_FILE)
